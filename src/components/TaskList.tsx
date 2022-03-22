e6f67ca32ff2926a53e3bf416acada2cc1f4e4e8
wellpinho <wellpinho@outlook.com>
2022-03-22 19:34:53 -0300

    🔨 REFACTOR:  return erro if newTaskTitle is empty

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return 'The input need a value'
+    }
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks((oldState) => {
+      setNewTaskTitle('')
+      return [...oldState, newTask];
+    })
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const markTasks = tasks.map((task) => {
+      return task.id === id ? {...task, isComplete: !task.isComplete} : task
+    })
+
+    setTasks(markTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(item => item.id !== id)
+
+    return setTasks(filteredTasks)
   }
 
   return (
